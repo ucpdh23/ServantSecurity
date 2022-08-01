3286774e19730041d7d6cb5f2fbf05cf41e2b509
xan <xan.xan@gmail.com>
2022-08-01 16:18:54 +0200

    adding support for multithreading

--- principal.py
+++ principal.py
@@ -1,26 +1,45 @@
 import sys
 import serial
 from actuator import Actuator
+import threading
+
+import ConfigParser
+
+from vertx import EventBus
 
 
-def principal():
-    actuator = Actuator()
-
+def serial_processor(ebus):
     _serial = serial.Serial(
-        port= '/dev/ttyAMA0',
-        baudrate = 9600,
+        port='/dev/ttyAMA0',
+        baudrate=9600,
         timeout=5
     )
 
     while True:
         x = _serial.readline()
-        parts = x.plit(b'\x00')
+        parts = x.split(b'\x00')
         for part in parts:
             action = part.decode()
             if action != "":
-                actuator.addItem(action[:3], action[3:])
+                ebus.send("event", body={'name': 'python', 'value': 'yes'})
+
+
+
+def principal(configFile):
+    config = ConfigParser.RawConfigParser()
+    config.read(configFile)
+
+    host = config['EventBus']['host']
+    port = config['EventBus']['port']
+
+    ebus = EventBus(host, port)
+    ebus.connect()
+
+    t = threading.threat(target= serial_processor, args=(ebus))
+    t.start()
+
 
 
 if __name__ == '__main__':
-    principal()
+    principal(sys.argv[1])
 
